14a672e345b4f5a91bcd7284362c5d9c96afaed1
Taouai <baha.edine.taouai@hp.com>
2024-12-27 22:24:23 +0100

    connecting to DB

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const connectDB = require('./Config/db');
 
 const app = express();
+
+// Connect DataBase
+connectDB();
 
 app.get('/',(req,res) =>res.send('API Running'));
 
